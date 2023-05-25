c2316a55b69be5e04c14dc4f3f7750ed23104b72
Sugam Singh <singhsugam065@gmail.com>
2023-05-25 18:22:15 +0530

    ADD: added 3d weather icons

--- lib/data/constants.dart
+++ lib/data/constants.dart
@@ -1,3 +1,1 @@
 const String API_KEY = '98c877a2acae8e3d98e695674104cc39';
-
-Author identity unknown *** Please tell me who you are. Run git config --global user.email "you@example.com" git config --global user.name "Your Name" to set your account's default identity. Omit --global to set the identity only in this repository. unable to auto-detect email address (got 'Anurag Verma@DESKTOP-AO5DDAV.(none)')
